4a4a134de5cc476c00ccf5337c3dc2a9bf0fb8ab
leyoncecharles <mlee99882@gmail.com>
2026-02-23 13:36:06 +0300

    added get products

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 from flask import *
 import pymysql
+import os
+
 app = Flask (__name__)
+app.config['UPLOAD_FOLDER'] = 'static/images'
+
 
 @app.route("/api/signup" , methods=["POST"])
 def signup():
@@ -46,7 +50,62 @@ def signin ():
     user=cursor.fetchone()
     connection.commit()
     return jsonify({"message":"Login successful","user":user})
+  
+@app.route("/api/add_product",methods=["POST"])
+def addProduct():
+   product_name = request.form['product_name']
+   product_description = request.form["product_description"]
+   product_category = request.form['product_category']
+   product_cost = request.form['product_cost']
+   product_image = request.files['product_image']
 
+   print(product_name,product_description,product_category,product_cost,product_image)
+
+#    get image
+   image_name = product_image.filename
+   print(image_name)
+
+#    save image to theimage folder  
+   file_path = os.path.join(app.config['UPLOAD_FOLDER'],image_name)
+   print(file_path)
+
+
+   product_image.save(file_path)
+
+#    create connection to the dbase
+   connection = pymysql.connect(host="localhost",user="root",password="",database="leyonce_sokogarden")
+   cursor= connection.cursor()
+   
+#    sql to execute
+   sql = "insert into product_details(product_name,product_description,product_category,product_cost,product_image) values (%s, %s, %s,%s,%s)"
+   data = (product_name,product_description,product_category,product_cost,image_name)
+#    execute query
+   cursor.execute(sql,data)
+
+#    save the data
+   connection.commit()
+    
+   return jsonify({"message":"product added successfully"})
+
+  
+@app.route("/api/get_products")
+def getProduct():
+   connection=pymysql.connect(host="localhost",user="root",password="",database="leyonce_sokogarden")
+
+   cursor=connection.cursor(pymysql.cursors.DictCursor)
+
+   sql="select * from product_details"
+
+   cursor.execute(sql)
+   if cursor.rowcount ==0:
+      return jsonify({"message":"Out of stock"})
+   else:
+      products=cursor.fetchall()
+      return jsonify(products)
+
+
+
+   
 
 if __name__ == "__main__": 
     app.run(debug=True)
